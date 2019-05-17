33cea38758e8c09dfe2433a0488d3c9a10ec84d9
jessun <jessun1990@gmail.com>
2019-05-17 20:21:43 +0800

    Update

--- sync/sync.go
+++ sync/sync.go
@@ -66,3 +66,52 @@ func ShowMutex() {
 	arithmetic.Wait()
 	fmt.Println("Arithmetic complete")
 }
+
+func ShowSyncCond() {
+	c := sync.NewCond(&sync.Mutex{})
+	queue := make([]interface{}, 0, 10)
+
+	removeFromQueue :=
+		func(delay time.Duration) {
+			time.Sleep(delay)
+			c.L.Lock()
+			queue = queue[1:]
+			fmt.Println("Removed from queue")
+			c.L.Unlock()
+			c.Signal()
+		}
+
+	for i := 0; i < 10; i++ {
+		c.L.Lock()
+		for len(queue) == 2 {
+			c.Wait()
+		}
+
+		fmt.Println("Adding to queue")
+		queue = append(queue, struct{}{})
+		go removeFromQueue(1 * time.Second)
+		c.L.Unlock()
+	}
+}
+
+//Sync.NewCond()
+
+//Sync.Once
+func ShowSyncOnce() {
+	var count int
+
+	increment := func() {
+		count++
+	}
+	var once sync.Once
+	var increments sync.WaitGroup
+	increments.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer increments.Done()
+			once.Do(increment)
+		}()
+	}
+	increments.Wait()
+	fmt.Printf("Count is %d\n", count)
+}
