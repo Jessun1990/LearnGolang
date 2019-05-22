2f438d1399385323209921573d8dc1907df74f13
jessun <jessun1990@gmail.com>
2019-05-22 14:01:59 +0800

    Update pipline

--- channel/pipline.go
+++ channel/pipline.go
@@ -26,3 +26,63 @@ func piplineExample() {
 	}
 
 }
+
+func piplineExample2() {
+	generator := func(done <-chan interface{}, integers ...int) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for _, i := range integers {
+				select {
+				case <-done:
+					return
+				case intStream <- i:
+				}
+			}
+		}()
+		return intStream
+	}
+
+	multiply := func(done <-chan interface{}, intStream <-chan int, multiplier int,
+	) <-chan int {
+		multipiedStream := make(chan int)
+		go func() {
+			defer close(multipiedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case multipiedStream <- i * multiplier:
+
+				}
+			}
+		}()
+		return intStream
+	}
+
+	add := func(done <-chan interface{}, intStream <-chan int, additive int) <-chan int {
+		addedStream := make(chan int)
+		go func() {
+			defer close(addedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case addedStream <- i + additive:
+				}
+			}
+		}()
+		return addedStream
+	}
+
+	// Pipline 最佳实践
+	done := make(chan interface{})
+	defer close(done)
+
+	intStream := generator(done, 1, 2, 3, 4)
+	pipline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipline {
+		fmt.Println(v)
+	}
+}
