1d214bb0d76c44f5ed3fcfc1275c7373f6ab4375
jessun <jessun1990@gmail.com>
2019-06-10 01:30:59 +0800

    update

--- chapter4/pipeline.go
+++ chapter4/pipeline.go
@@ -1,6 +1,9 @@
 package chapter4
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func piplineExample() {
 	multiply := func(values []int, multiplier int) []int {
@@ -80,5 +83,98 @@ func piplineExample2() {
 
 	for v := range pipline {
 		fmt.Println(v)
+	}
+}
+
+// 生成器举例
+// go test ./channel -run TestPiplineExample3 -v -count=1
+func piplineExample3() {
+	repeat := func(done <-chan interface{}, values ...interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(done <-chan interface{}, valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	for num := range take(done, repeat(done, 1), 10) {
+		fmt.Printf("%d\n", num)
+	}
+
+}
+
+func piplineExample4() {
+	repeatFn := func(done <-chan interface{}, fn func() interface{},
+	) <-chan interface{} {
+
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(done <-chan interface{}, valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	rand := func() interface{} { return rand.Int() }
+	for num := range take(done, repeatFn(done, rand), 10) {
+		fmt.Printf("%d\n", num)
 	}
 }
