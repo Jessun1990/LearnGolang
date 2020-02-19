3f448c0c21b4d0f801bf74ccb3b0eb1f07fd5ef5
jessun <jessun1990@gmail.com>
2020-02-19 12:36:48 +0800

    Update: main.go output info

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module concurrency_in_go
 
 go 1.13
-
-require github.com/hsyan2008/go-logger v0.0.0-20190518062908-30b005d1c42b
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
 	// the main func is nil
+
+	fmt.Println("<< Go语言并发之道 >> 中代码示例和重点内容整理")
 }
